e385d3c04c746bd203b74da1c5734b5189f47402
Lilly <83720082+LOLthecool@users.noreply.github.com>
2026-01-22 15:36:13 +0000

    rate limit debugging

--- src/rate_limit.rs
+++ src/rate_limit.rs
@@ -26,6 +26,8 @@ pub async fn rate_limit<
         .await
         .unwrap();
 
+    dbg!(addr);
+
     if let Some(addr_history) = state.request_history.read().await.get(&addr.ip()) {
         let mut requests_last_minute = 0;
         let mut requests_last_hour = 0;
@@ -35,8 +37,10 @@ pub async fn rate_limit<
 
         let comparison_time = SystemTime::now();
 
-        while let Some(x) = addr_history.iter().last() {
-            if comparison_time.duration_since(*x).unwrap_or_default() < Duration::from_hours(24) {
+        while let Some(x) = addr_history.iter().rev().next() {
+            if dbg!(comparison_time.duration_since(*x)).unwrap_or_default()
+                < Duration::from_hours(24)
+            {
                 break;
             }
             addr_history.pop_back();
@@ -48,21 +52,27 @@ pub async fn rate_limit<
                 .duration_since(*req_time)
                 .unwrap_or_default();
             if time < Duration::from_mins(1) {
+                println!("request minute");
                 requests_last_minute += 1;
             }
             if time < Duration::from_hours(1) {
+                println!("request hour");
                 requests_last_hour += 1;
             }
+            println!("request day");
             requests_last_day += 1;
         }
 
         if requests_last_minute > MINUTE_LIMIT {
+            println!("limit minute");
             return Err(StatusCode::TOO_MANY_REQUESTS);
         }
         if requests_last_hour > HOUR_LIMIT {
+            println!("limit hour");
             return Err(StatusCode::TOO_MANY_REQUESTS);
         }
         if requests_last_day > DAY_LIMIT {
+            println!("limit day");
             return Err(StatusCode::TOO_MANY_REQUESTS);
         }
     } else {
