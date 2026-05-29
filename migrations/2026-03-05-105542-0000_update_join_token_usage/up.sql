a644207a7ec28798776882438fc58b5680d1ddb8
Lilly <83720082+LOLthecool@users.noreply.github.com>
2026-05-29 11:11:32 +0100

    fix migrations

--- migrations/2026-03-05-105542-0000_update_join_token_usage/up.sql
+++ migrations/2026-03-05-105542-0000_update_join_token_usage/up.sql
@@ -1,6 +1,1 @@
-ALTER TABLE instances DROP COLUMN "client_token" CASCADE;
-ALTER TABLE instances ADD COLUMN "ip" INET NOT NULL;
-ALTER TABLE instances ADD COLUMN "port" INTEGER NOT NULL;
-ALTER TABLE users ADD COLUMN "identifier" BYTEA NULL;
-
-CREATE INDEX "users_identifier_idx" ON "users" ("identifier");
+ALTER TABLE instances ADD COLUMN TOKEN_VALID BOOLEAN NOT NULL DEFAULT FALSE;
---
+++ migrations/2026-05-29-100330-0000_new_instances/up.sql
@@ -0,0 +1,7 @@
+ALTER TABLE instances DROP COLUMN TOKEN_VALID CASCADE;
+ALTER TABLE instances DROP COLUMN "client_token" CASCADE;
+ALTER TABLE instances ADD COLUMN "ip" INET NOT NULL;
+ALTER TABLE instances ADD COLUMN "port" INTEGER NOT NULL;
+ALTER TABLE users ADD COLUMN "identifier" BYTEA NULL;
+
+CREATE INDEX "users_identifier_idx" ON "users" ("identifier");
